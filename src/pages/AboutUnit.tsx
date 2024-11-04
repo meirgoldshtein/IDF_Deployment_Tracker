736605c74771a236e1fe8da030cee03321765767
meirgoldshtein <m0548491057@gmail.com>
2024-11-04 17:57:59 +0200

    Preparation for retrieving the parameter in the unit private component

--- src/pages/AboutUnit.tsx
+++ src/pages/AboutUnit.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import UnitsDetails from '../components/UnitsDetails'
+import { useParams } from 'react-router-dom';
 
 export default function AboutUnit() {
+    const { unit } = useParams();
   return (
-    <UnitsDetails/>
+    <div>
+        <h2> שם היחידה: {unit}</h2>
+        <UnitsDetails/>
+    </div>
+
+
   )
 }
--- src/pages/UnitsPage.tsx
+++ src/pages/UnitsPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import UnitList from '../components/UnitList'
+import { useParams } from 'react-router-dom';
 
 export default function UnitsPage() {
+    
   return (
     <div>
+        
         <UnitList/>
     </div>
   )
