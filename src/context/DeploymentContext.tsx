da52d6609062a64d0eb08777fdb9c53a6b40b223
meirgoldshtein <m0548491057@gmail.com>
2024-11-04 17:26:21 +0200

    Added a global state that holds all the units

--- src/context/DeploymentContext.tsx
+++ src/context/DeploymentContext.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { createContext, useState } from "react";
+import React from "react";
+import { DeploymentContextType, DeploymentProviderProps } from "../types/deployment";
 
-export default function DeploymentContext() {
-  return (
-    <div>DeploymentContext</div>
-  )
-}
+
+
+const DeploymentContext = createContext<DeploymentContextType | undefined>(undefined);
+
+const DeploymentProvider: React.FC<DeploymentProviderProps> = ({children}) => {
+
+    const [units, setUnits] = useState({
+        Golani: 'Idle', Paratroopers: 'Idle',
+        Givati: 'Idle',
+        gunners: 'Idle',
+        tankers: 'Idle',
+        navy: 'Idle',
+
+    });
+
+    const setUnitStatus = (unit: string, status: string) => {
+
+        setUnits({ ...units, [unit]: status });
+
+    };
+    return (
+        <DeploymentContext.Provider value={{ units, setUnitStatus }}>
+            {children}
+        </DeploymentContext.Provider>
+    );
+};
+export { DeploymentContext, DeploymentProvider };
